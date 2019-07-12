5a8e17a799e26db461604004eac0d6e52a211785
sdilbaz <serdarcan.dilbaz@gmail.com>
2019-07-12 11:04:10 -0400

    Update reddit-collection.py

--- reddit-collection.py
+++ reddit-collection.py
@@ -7,14 +7,15 @@ Created on Wed Jul 10 21:00:58 2019
 
 import praw
 from praw.models import MoreComments
-import pandas as pd
-import datetime as dt
 import json
-from urllib.request import urlopen, build_opener
+from urllib.request import build_opener
 from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
 import time
 import os
+from multiprocessing import Process, Manager
+
+depth_limit=10
 
 def single_node(comment):
     sub_tree=ET.Element("response")
@@ -52,6 +53,8 @@ def generate_tree(comment):
     try:
         if comment.ups<3:
             return None
+        if comment.depth>depth_limit:
+            return None
     except:
         return None
     comment=comment.refresh()
@@ -60,65 +63,62 @@ def generate_tree(comment):
     else:
         sub_tree=single_node(comment)        
         for reply in comment.replies:
-            reply_tree=generate_tree(reddit.comment(reply))
+            reply_tree=generate_tree(reply)
             if not reply_tree is None:
                 sub_tree.insert(-1,reply_tree)    
         return sub_tree
 
+def retrieve_subreddit_list():
+    if os.path.isfile("subreddit-list.txt"):
+        with open("subreddit-list.txt","r") as file:
+            subreddits=file.read().split('\n')
+    else:
+        subreddits=[]    
+        opener = build_opener()
         
-     
-if not os.path.isfile("subreddit-list.txt"):
-    subreddits=[]    
-    opener = build_opener()
-    
-    main_url="http://redditlist.com/sfw"
-    response=opener.open(main_url)
-    
-    page_html = response.read()
-    soup=BeautifulSoup(page_html,'lxml')
-    
-    mydivs = soup.findAll("div", {"class": "listing-item"})
-    for div in mydivs[int(len(mydivs)/3):-int(len(mydivs)/3)]:
-        subreddits.append(div.attrs['data-target-subreddit'])
-        
-    for page_no in range(2,32):
-#        print(page_no)
-        
-        main_url="http://redditlist.com/sfw?page="+str(page_no)
+        main_url="http://redditlist.com/sfw"
         response=opener.open(main_url)
         
         page_html = response.read()
         soup=BeautifulSoup(page_html,'lxml')
-    
+        
         mydivs = soup.findAll("div", {"class": "listing-item"})
         for div in mydivs[int(len(mydivs)/3):-int(len(mydivs)/3)]:
             subreddits.append(div.attrs['data-target-subreddit'])
+            
+        for page_no in range(2,32):
+    #        print(page_no)
+            
+            main_url="http://redditlist.com/sfw?page="+str(page_no)
+            response=opener.open(main_url)
+            
+            page_html = response.read()
+            soup=BeautifulSoup(page_html,'lxml')
+        
+            mydivs = soup.findAll("div", {"class": "listing-item"})
+            for div in mydivs[int(len(mydivs)/3):-int(len(mydivs)/3)]:
+                subreddits.append(div.attrs['data-target-subreddit'])
+        
+        with open("subreddit-list.txt","w") as file:
+            file.write('\n'.join(subreddits))
     
-    with open("subreddit-list.txt","w") as file:
-        file.write('\n'.join(subreddits))
+    return subreddits
 
-else:
-    with open("subreddit-list.txt","r") as file:
-        subreddits=file.read().split('\n')
-
-with open(r"D:\Conversation_Code\reddit-credentials.json", "r") as file:
-    creds = json.load(file)
-client_id=creds['client_id'],
-client_secret=creds['client_secret'],
-user_agent=creds['user_agent'],
-username=creds['username'],
-password=creds['password']
-
-reddit = praw.Reddit(client_id=creds['client_id'],
-                     client_secret=creds['client_secret'],
-                     user_agent=creds['user_agent'],
-                     username=creds['username'],
-                     password=creds['password'])
-
-for subr in subreddits:
-    subreddit = reddit.subreddit(subr)
-    top_subreddit = subreddit.top(limit=None)
-    for submission in top_subreddit:
+def explore_submission(cred_id,mlist):
+    while len(mlist):
+        subm_id=mlist.pop()
+        print(subm_id)
+        with open(r"D:\Conversation_Code\credentials\reddit-credentials-"+str(cred_id)+".json", "r") as file:
+            creds = json.load(file)
+    
+        reddit = praw.Reddit(client_id=creds['client_id'],
+                         client_secret=creds['client_secret'],
+                         user_agent=creds['user_agent'],
+                         username=creds['username'],
+                         password=creds['password'])
+        
+        submission=reddit.submission(id=subm_id)
+        
         root = ET.Element("submission")
         try:
             author=submission.author
@@ -136,7 +136,6 @@ for subr in subreddits:
             ET.SubElement(root,"post_subreddit").text=post_subreddit
         except:
             pass
-        
         try:
             if not submission.edited:
                 post_time=time.ctime(submission.created)
@@ -165,19 +164,72 @@ for subr in subreddits:
             ET.SubElement(root,"post_body").text=post_body
         except:
             pass
-
+    
         print('Adding more comments')
         submission.comments.replace_more(limit=256)#None)
         print('Added more comments')
-
+    
         for comment in submission.comments:
             if comment.stickied or isinstance(comment, MoreComments):
                 continue
             generated=generate_tree(comment)
             if not generated is None:
-                root.insert(-1,generate_tree(comment))
+                root.insert(-1,generated)
             
         tree = ET.ElementTree(root)
         tree.write(submission.id+".xml")
 
+def list_subm(cred_id,subreddit_list,mlist):
+    while len(subreddit_list):
+        subr=subreddit_list.pop()
+        print(subr)
+        with open(r"D:\Conversation_Code\credentials\reddit-credentials-"+str(cred_id)+".json", "r") as file:
+            creds = json.load(file)
+    
+        reddit = praw.Reddit(client_id=creds['client_id'],
+                         client_secret=creds['client_secret'],
+                         user_agent=creds['user_agent'],
+                         username=creds['username'],
+                         password=creds['password'])
+        
+        subreddit = reddit.subreddit(subr)
+        top_subreddit = subreddit.top(limit=None)
+        for submission in top_subreddit:
+            mlist.append(submission)
 
+if __name__=='__main__':
+    account_num=7
+    manager=Manager()
+    
+    if os.path.isfile("submission_list.txt"):
+        with open("submission_list.txt","r") as file:
+            submission_list=file.read().split('\n')
+        print('Read Submission List')
+        mlist=manager.list(submission_list)
+    else:
+        print('Generating Submission List')
+        subreddits=retrieve_subreddit_list()
+        subreddit_list=manager.list(subreddits)
+        mlist=manager.list()
+        processes=[]
+        for cred_id in range(account_num):
+            p=Process(target=list_subm,args=[cred_id,subreddit_list,mlist])
+            processes.append(p)
+            p.start()
+
+        while any([p.is_alive() for p in processes]):
+            time.sleep(60)
+
+        [p.join() for p in processes]
+        with open("submission_list.txt","w") as file:
+            file.write('\n'.join(list(subreddit_list)))
+        
+    
+    processes=[]
+    for cred_id in range(account_num):
+        p=Process(target=explore_submission,args=[cred_id,mlist])
+        processes.append(p)
+        p.start()
+    [p.join() for p in processes]
+        
+    
